06ad3028c102dd20690c97998283fdaee1357a79
IDFCSGFY <idfcsgfy@gmail.com>
2025-01-25 11:32:06 +0700

    logs and profile-depending port done

--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -18,13 +20,17 @@ public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository repository;
 
+    Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
+
     public StudentServiceImpl(StudentRepository repository) {
         this.repository = repository;
     }
 
     @Override
     public Student post(Student student) {
+        logger.info("StudentService post method is invoked");
         if (student == null) {
+            logger.error("Student object is null");
             return null;
         }
         return repository.save(student);
@@ -32,12 +38,15 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student findById(long id) {
+        logger.info("StudentService findById method is invoked");
         return repository.findById(id).get();
     }
 
     @Override
     public Student edit(Student student) {
+        logger.info("StudentService edit method is invoked");
         if (student == null) {
+            logger.info("Student object is null");
             return null;
         }
         return repository.save(student);
@@ -45,16 +54,19 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public void deleteById(long id) {
+        logger.info("StudentService deleteById method is invoked");
         repository.deleteById(id);
     }
 
     @Override
     public Collection<Student> findAll() {
+        logger.info("StudentService findAll method is invoked");
         return List.copyOf(repository.findAll());
     }
 
     @Override
     public Collection<Student> filterByAge(int age) {
+        logger.info("StudentService filterByAge method is invoked");
         return repository.findAll().stream()
                 .filter(e -> e.getAge() == age)
                 .collect(Collectors.toList());
@@ -62,25 +74,30 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Collection<Student> filterByAge(int age, int ageMax) {
+        logger.info("StudentService filterByAge method is invoked");
         return List.copyOf(repository.findByAgeBetween(age, ageMax));
     }
 
     public Faculty findFacultyOfStudentById(long id) {
+        logger.info("StudentService findFacultyOfStudentById method is invoked");
         return findById(id).getFaculty();
     }
 
     @Override
     public Integer countAll() {
+        logger.info("StudentService countAll method is invoked");
         return repository.countAllStudents();
     }
 
     @Override
     public Double getAverageAgeOfAllStudents() {
+        logger.info("StudentService getAverageAgeOfAllStudents method is invoked");
         return repository.getAverageAgeOfAllStudents();
     }
 
     @Override
     public Collection<Student> getLastFiveStudents() {
+        logger.info("StudentService getLastFiveStudents method is invoked");
         return List.copyOf(repository.getLastFiveStudents());
     }
 }
